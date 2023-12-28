361b6dc6dcdd44d0fab18d63f4edc29f62cbd3fb
Abhishek p p <abhishekkanichery@gmail.com>
2023-12-28 12:34:31 +0530

    ejs sign up form added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const path = require("path");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Sign up" });
+});
+
+router.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.render("user",{registerOrNot:"Sucessfull"});
 });
 
 module.exports = router;
